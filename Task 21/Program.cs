c066bdec10e814f4ccd7962bcf15de06d40e2773
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-07 17:04:23 +0300

    Task 21

---
+++ Task 21/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] point = new int[6];
+System.Console.WriteLine("Введите x1: ");
+point[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите y1: ");
+point[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите z1: ");
+point[2] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите x2: ");
+point[3] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите y2: ");
+point[4] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите z2: ");
+point[5] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Math.Sqrt(Math.Pow(point[3] - point[0], 2) + Math.Pow(point[4] - point[1], 2) + Math.Pow(point[5] - point[2], 2)));
+
