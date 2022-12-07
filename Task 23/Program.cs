60495f3424a3a617e4dfe7836c8d82af288f514d
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-07 17:23:35 +0300

    Task 23

---
+++ Task 23/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число от 1 до бесконечности: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1)
+{
+    Console.WriteLine("Число меньше 1");
+}
+else for (int i = 1; i <= n; i++)
+{
+    System.Console.Write($"{i * i * i} ");
+}
+
