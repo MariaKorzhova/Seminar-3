95663e5a9e9846130b8bd8bc83b46dc697ff2f41
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-07 16:50:52 +0300

    Task 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string n = Convert.ToString(Console.ReadLine());
+if (n.Length == 5)
+{
+    if (n[0] == n[4] && n[1] == n[3])
+    {
+        System.Console.WriteLine($"Число {n} является палиндромом");
+    }
+    else 
+    {
+        System.Console.WriteLine($"Число {n} НЕ является палиндромом");
+    }
+}
+else 
+{
+    System.Console.WriteLine($"Число {n} НЕ является пятизначным");
+}
